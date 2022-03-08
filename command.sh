a62e529c85600f0ca680d30df0591eda08cae909
SergiosCode <streameraudio@yahoo.com>
2022-03-08 00:14:29 -0800

    added command.sh file

---
+++ command.sh
@@ -0,0 +1,9 @@
+# https://github.com/SergiosCode/propeller-task1
+
+1. npx create-create-app .
+2. git init  git clone /path/to/repository
+3. git checkout -b "logo_updated"
+4. git add *
+5. git commit -m "updated logo and link"
+6. gh pr create
+7. gh pr merge -m
